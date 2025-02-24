c9ca1dea3184138742f6005fcbbe418ca5ebc031
camila802 <mtautivag@unal.edu.co>
2025-02-23 19:24:30 -0500

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -13,3 +13,17 @@ def compras_por_pais(pais):
 pais = 'United Kingdom'
 print(f'Compras realizadas en {pais}: {compras_por_pais(pais)}')
 
+##Ejercicio 2
+
+def compras_por_medio_pago(medio_pago):
+    total_compras = 0
+    with open('files/SalesJan2009.csv', 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader:
+            if row[3] == medio_pago:
+                total_compras += 1
+    return total_compras
+
+medio_pago = 'Visa'
+print(f'Compras realizadas con {medio_pago}: {compras_por_medio_pago(medio_pago)}')
